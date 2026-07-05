af4d0d2a6f58deb4b0b5b06ccb657e5f896d790e
tom <tom@tom.com>
2026-07-05 15:58:08 +0300

    small modifications

--- example.py
+++ example.py
@@ -11,12 +11,13 @@ if __name__ == "__main__":
     logger = logging.getLogger()
     logging.basicConfig(encoding='utf-8')
     logger.setLevel('DEBUG')
-    mocap = SimpleMocap(local_ip="10.240.10.123", server_ip="10.240.10.0")
+    mocap = SimpleMocap(local_ip="10.240.10.128", server_ip="10.240.10.10")
 
     for i in range(5):
         print(f'Frame number: {mocap.get_frame_number()}')                      # Get frame number
-        print(f'Target: {mocap.get_location("IOT_car")}')                        # Get rigid body "Target" by name
+        # print(f'Target: {mocap.get_location("IOT_car")}')                        # Get rigid body "Target" by name
         print(f'Target: {mocap.get_location(600)}')                             # Get rigid body "Traget" by ID
+        print(f'Target: {mocap.get_location(601)}')                             # Get rigid body "Traget" by ID
         sleep(1)
 
     mocap.shutdown()        # Always call shutdown before script end
--- instructions.md
+++ instructions.md
@@ -1,8 +1,10 @@
 steps - 
-1. connect to the CIS_ROBOTS wifi network. password will be emailed.
+1. connect to the mocap wifi network. SSID/password will be emailed.
 2. clone the repo https://github.com/AlonGil-Ad/simple_mocap
 3. make sure you know the NAME or ID of your "asssets" (rigid bodies)
 4. make sure the asset/s are in the tracked area (cameras) 
-5. run example.py, if frame number is not advancing, you are not connected to the system. 
-6. notice - the angles are given in Quaternions , you can use any python library to convert to roll,pitch,yaw
+5. open  example.py, update your computer IP , and asset number/s and run
+6. first 3 numbers are xyz in meters. notice the axes are Y-UP NOT Z-UP. there is a blue L on the floor to indicate X,Y,Z=0,0,0
+7. second array of 4 numbers - the angles given in Quaternions , you can use any python library to convert to roll,pitch,yaw
+
 
